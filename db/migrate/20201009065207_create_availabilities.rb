7c372c5e089e456071d854d3e4db39abe3c0789c
Jonathan Chan <tmxistz@gmail.com>
2020-10-09 16:16:47 +0900

    added migration for availabilities

--- db/migrate/20201009065207_create_availabilities.rb
+++ db/migrate/20201009065207_create_availabilities.rb
@@ -1,6 +1,10 @@
 class CreateAvailabilities < ActiveRecord::Migration[6.0]
   def change
     create_table :availabilities do |t|
+      t.string :user_id, null: false
+      t.date :date, null: false
+      t.time :from, null: false
+      t.time :to, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,5 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_10_09_065207) do
+
+  create_table "availabilities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "user_id", null: false
+    t.date "date", null: false
+    t.time "from", null: false
+    t.time "to", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
