2272fa1ac7d06313d5078fa9027446285229623f
Jonathan Chan <tmxistz@gmail.com>
2020-10-09 23:42:56 +0900

    added availability validations

--- spec/models/availability_spec.rb
+++ spec/models/availability_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe Availability, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  dummy_id = 'dummy_id'
+
+  it 'returns error when it is a sunday' do
+    from = Time.current.change(day: 11, month: 10, year: 2020, hour: 10, min: 0)
+
+    expect { Availability.create!(user_id: dummy_id, from: from) }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'returns error when start time is before 1000 on a weekday' do
+    from = Time.current.change(day: 12, month: 10, year: 2020, hour: 9, min: 0)
+
+    expect { Availability.create!(user_id: dummy_id, from: from) }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'returns error when start time is after 1730 on a weekday' do
+    from = Time.current.change(day: 12, month: 10, year: 2020, hour: 18, min: 0)
+
+    expect { Availability.create!(user_id: dummy_id, from: from) }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'returns error when start time is before 1100 on a saturday' do
+    from = Time.current.change(day: 10, month: 10, year: 2020, hour: 10, min: 0)
+
+    expect { Availability.create!(user_id: dummy_id, from: from) }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'returns error when start time is after 1430 on a saturday' do
+    from = Time.current.change(day: 10, month: 10, year: 2020, hour: 15, min: 0)
+
+    expect { Availability.create!(user_id: dummy_id, from: from) }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'returns error when start time does not begin at the hour or half hour' do
+    from = Time.current.change(day: 10, month: 10, year: 2020, hour: 14, min: 15)
+
+    expect { Availability.create!(user_id: dummy_id, from: from) }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'creates availability when from begins at the hour for a weekday' do
+    from = Time.current.change(day: 12, month: 10, year: 2020, hour: 10, min: 0)
+
+    Availability.create!(user_id: dummy_id, from: from)
+    expect(Availability.count).to eq 1
+  end
+
+  it 'creates availability when from begins at the half hour for a weekday' do
+    from = Time.current.change(day: 12, month: 10, year: 2020, hour: 17, min: 30)
+
+    Availability.create!(user_id: dummy_id, from: from)
+    expect(Availability.count).to eq 1
+  end
+
+  it 'creates availability when from begins at the hour for a saturday' do
+    from = Time.current.change(day: 10, month: 10, year: 2020, hour: 11, min: 0o0)
+
+    Availability.create!(user_id: dummy_id, from: from)
+    expect(Availability.count).to eq 1
+  end
+
+  it 'creates availability when from begins at the half hour for a saturday' do
+    from = Time.current.change(day: 10, month: 10, year: 2020, hour: 14, min: 30)
+
+    Availability.create!(user_id: dummy_id, from: from)
+    expect(Availability.count).to eq 1
+  end
 end
