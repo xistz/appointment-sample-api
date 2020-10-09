ed8ce8e19799c4ad34b17e58ecd4e4ed710de275
Jonathan Chan <tmxistz@gmail.com>
2020-10-09 15:56:12 +0900

    added register controller

---
+++ app/controllers/register_controller.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+class RegisterController < SecuredController
+  before_action :is_new_user
+
+  def new
+    role = new_params[:role]
+
+    UserService::Register.call(@user_id, role)
+
+    response = {
+      message: "registered as a #{role}"
+    }
+
+    render json: response, status: :created
+  rescue StandardError => e
+    puts e
+    response = {
+      message: e.message
+    }
+
+    render json: response, status: :internal_server_error
+  end
+
+  private
+
+  def is_new_user
+    if @user_roles.present?
+      response = {
+        message: "user has already registered as a #{@user_roles.first}"
+      }
+
+      render json: response, status: :bad_request
+    end
+  end
+
+  def new_params
+    params.require(:register).permit(:role)
+  end
+end
