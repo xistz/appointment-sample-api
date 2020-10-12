0ffdb6886bf1f4b5453e82d1f66188c0cea8e12d
Jonathan Chan <tmxistz@gmail.com>
2020-10-12 16:00:26 +0900

    removed base ApplicationService

--- app/services/application_service.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# ApplicationService defines base class for other services to inherit
-class ApplicationService
-  def self.call(*args, &block)
-    new(*args, &block).call
-  end
-end
