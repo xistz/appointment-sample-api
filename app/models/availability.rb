f8503b70acd1fef9915257e9d1f9e41030ee6da4
Jonathan Chan <tmxistz@gmail.com>
2020-10-13 23:49:35 +0900

    added dependent destroy

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,5 +1,5 @@
 class Availability < ApplicationRecord
-  has_one :appointment
+  has_one :appointment, dependent: :destroy
 
   validates :user_id, presence: true
   validates :from, presence: true
