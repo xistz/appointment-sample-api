7ce421923d1d295768f318fbb4dab15fd93b55eb
Jonathan Chan <tmxistz@gmail.com>
2020-10-14 00:14:40 +0900

    added search at time to AvailabilityService::Search

--- app/services/availability_service/search.rb
+++ app/services/availability_service/search.rb
@@ -2,13 +2,18 @@
 
 module AvailabilityService
   class Search
-    def initialize(from:, to:)
+    def initialize(from: nil, to: nil, at: nil)
       @from = from
       @to = to
+      @at = at
     end
 
     def execute
-      availabilities = Availability.where(from: (@from..@to))
+      availabilities = if @from.present? && @to.present?
+        Availability.where(from: (@from..@to))
+      else
+        Availability.where(from: @at)
+      end
 
       availabilities
     end
