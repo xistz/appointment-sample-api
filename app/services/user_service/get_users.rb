0d9834ab28fd7320413b9cd54cd099b96a688955
Jonathan Chan <tmxistz@gmail.com>
2020-10-14 22:39:02 +0900

    refactored UserService::GetUsers

--- app/services/user_service/get_users.rb
+++ app/services/user_service/get_users.rb
@@ -1,7 +1,8 @@
 module UserService
-  class GetUsers
+  class GetUsers < Base
     def initialize(user_ids:)
       @user_ids = user_ids
+      @token = get_token
     end
 
     def execute
@@ -11,7 +12,43 @@ module UserService
     private
 
     def get_users
-      Auth0.get_users(@user_ids)
+      escaped_user_ids = @user_ids.map { |user_id| CGI.escape(user_id) }
+
+      url = URI("https://#{ENV['AUTH0_DOMAIN']}/api/v2/users")
+      params =  {
+        fields: 'user_id,name,picture',
+        include_fields: true,
+        search_engine: 'v3',
+        q: "(#{escaped_user_ids.join(' OR ')})"
+      }
+      url.query = URI.encode_www_form(params)
+
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      request = Net::HTTP::Get.new(url)
+      request['authorization'] = "Bearer #{@token}"
+
+      response = http.request(request)
+      body = response.read_body
+
+      results = JSON.parse(body)
+
+      # convert array of users to hash
+      users = {}
+      results.each do |result|
+        user_id = result['user_id']
+        name = result['name']
+        picture = result['picture']
+
+        users[user_id] = {
+          name: name,
+          picture: picture
+        }
+      end
+
+      users
     end
   end
 end
