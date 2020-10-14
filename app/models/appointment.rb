f2a8305d005d8c4d1f61ccf0e7c9d4b9a79ae13c
Jonathan Chan <tmxistz@gmail.com>
2020-10-14 16:30:04 +0900

    delegated appointments fp_id and from to availabilities

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,7 @@ class Appointment < ApplicationRecord
   belongs_to :availability
 
   validates :client_id, presence: true
+
+  delegate :from, to: :availability
+  delegate :fp_id, to: :availability
 end
