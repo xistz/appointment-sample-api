1b5bb0b1e3f0bd57174fc77637eb09dd6e6c6274
Jonathan Chan <tmxistz@gmail.com>
2020-10-16 16:44:56 +0900

    removed debug puts

--- app/serializers/availability_serializer/search_by_time.rb
+++ app/serializers/availability_serializer/search_by_time.rb
@@ -4,7 +4,6 @@ module AvailabilitySerializer
     set_type :availability
 
     attribute :fp_name do |availability, params|
-      puts params[:users]
       params[:users][availability.fp_id][:name]
     end
 
