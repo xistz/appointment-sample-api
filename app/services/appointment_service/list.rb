a4adcb3621a1f2de2838e8e75ae33903f80368a1
Jonathan Chan <tmxistz@gmail.com>
2020-10-16 17:59:07 +0900

    added order

--- app/services/appointment_service/list.rb
+++ app/services/appointment_service/list.rb
@@ -9,7 +9,7 @@ module AppointmentService
     end
 
     def execute
-      appoinments_at_datetime = Appointment.joins(:availability).where(availabilities: { from: (@from..@to) })
+      appoinments_at_datetime = Appointment.joins(:availability).where(availabilities: { from: (@from..@to) }).order('availabilities.from')
       user_appointments = appoinments_at_datetime.where(client_id: @user_id).or(appoinments_at_datetime.where(availabilities: { fp_id: @user_id }))
 
       user_appointments
